a6ce353055de3d2ce8129b6e3e06a423eb1b2663
Kodai <kodai.ninja@gmail.com>
2019-10-24 07:13:00 +0800

    SSS

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="post-card content-box" :class="{'post-card--has-poster' : post.poster}">
+  <div class="post-card content-box gradient-border" :class="{'post-card--has-poster' : post.poster}">
     <div class="post-card__header">
       <g-image alt="Cover image" v-if="post.coverImage" class="post-card__image" :src="post.coverImage" />
     </div>

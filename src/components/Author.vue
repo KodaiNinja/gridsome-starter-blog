f25a50a08703ff18b9adc95ec86d8b642c5e9482
Kodai <kodai.ninja@gmail.com>
2019-10-23 20:03:17 +0800

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -8,12 +8,12 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			Life is about learning; when you stop learning, you die. Tom Clancy
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//twitter.com/KodaiNinja">Follow on Twitter</a>
+			<a href="//github.com/KodaiNinja">GitHub</a>
 		</p>
 
 	</div>
